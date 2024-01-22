bcf36b4ece90403fb7b25773a4cba39b09992879
viswasarathi <viswasarathi03@gmail.com>
2024-01-22 11:54:42 +0530

    create a setup.py file

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+        THIS FUNCTION WILL RETURN THE LIST OF REQUIREMENTS
+    '''
+    HYPHEN_DOT_E = "-e ."
+
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPHEN_DOT_E in requirements:
+            requirements.remove(HYPHEN_DOT_E)
+
+    return requirements
+
+setup(
+    name="mlproject",
+    version='0.0.1',
+    author="viswa",
+    author_email="viswasarathi03@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)
